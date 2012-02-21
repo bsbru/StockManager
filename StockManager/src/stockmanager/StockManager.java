3b7afe965f0f5a179835c5ccf910417f6fb313bf
Janie <aj.sinclair@tiscali.co.uk>
2012-02-21 15:51:42 +0000

    Testing adding the package

---
+++ StockManager/src/stockmanager/StockManager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockmanager;
+
+/**
+ *
+ * @author Ajs
+ */
+public class StockManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("yaytest");
+    }
+}
